bf8bfbbd416e279459f6334378a0f1158af2f09b
Rafael Cesar <rafaelcesar988@gmail.com>
2023-03-08 13:48:28 -0300

    0.1

---
+++ main.js
@@ -0,0 +1,61 @@
+function getNameDayWeek(dayWeek) {
+    let dayWeekText = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+
+    return dayWeekText[dayWeek]
+}
+
+function getNameMonth(numberMonth) {
+    let nameMonth = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+    return nameMonth[numberMonth];
+}
+
+function zeroLeft(num) {
+    return num >= 10 ? num : `0${num}`;
+}
+
+function createDate() {
+    const now = new Date();
+    const dayWeek = now.getDay();
+    const numMonth = now.getMonth();
+
+    const nameDay = getNameDayWeek(dayWeek);
+    const nameMonth = getNameMonth(numMonth);
+
+    return (`${nameDay}, ${nameMonth} ${now.getDate()}, ${now.getFullYear()} - ${zeroLeft(now.getHours())}:${zeroLeft(now.getMinutes())}:${zeroLeft(now.getSeconds())}`)
+}
+
+function nameDay() {
+    const day = new Date();
+    const dayWeek = day.getDay();
+
+    return getNameDayWeek(dayWeek);
+}
+
+function nameDate() {
+    const data = new Date();
+
+    return data.toLocaleDateString('en', {dateStyle: 'long'})
+}
+
+function timeNow() {
+    const time = new Date();
+    
+    return `${zeroLeft(time.getHours())}:${zeroLeft(time.getMinutes())}:${zeroLeft(time.getSeconds())}`
+}
+
+
+function upgradeDate(){
+    const day = document.querySelector('#day');
+    const data = document.querySelector('#date');
+    const clock = document.querySelector('#clock');
+
+    day.innerHTML = nameDay();
+    data.innerHTML = nameDate();
+    clock.innerHTML = timeNow();
+}
+
+upgradeDate();
+
+setInterval(upgradeDate,1000)
+
